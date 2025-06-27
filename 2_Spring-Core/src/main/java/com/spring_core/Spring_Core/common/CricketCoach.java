3a43ac6f0ca8aa03fbbac54c52a84f9aadaf5ad2
Mayur3sh007 <mayureshc007@gmail.com>
2025-06-27 22:00:28 +0530

    Using Primary Annotation

--- 2_Spring-Core/src/main/java/com/spring_core/Spring_Core/common/CricketCoach.java
+++ 2_Spring-Core/src/main/java/com/spring_core/Spring_Core/common/CricketCoach.java
@@ -1,8 +1,10 @@
 package com.spring_core.Spring_Core.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component  //This annotation makes this class available for Dependency Injection
+@Primary    //It makes this class a priority like a Default value if Qualifier is not used --> Multiple primary gives error
 public class CricketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
